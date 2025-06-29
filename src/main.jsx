63ae317df29e72bfdad7b43f2a633c82b56bde94
dev-Chaves <chavesprogrammer@gmail.com>
2025-06-29 17:47:12 -0300

    mudando as ordems do import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import LandingPage from './pages/LandingPage.jsx'
 import './index.css'
-import App from './pages/LandingPage.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LandingPage />
   </StrictMode>,
 )
