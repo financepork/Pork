6c017e23798f506e7f141387054bf7c1d14c61da
dev-Chaves <chavesprogrammer@gmail.com>
2025-06-30 19:10:55 -0300

    add rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './pages/Register.jsx'
 import LandingPage from './pages/LandingPage.jsx'
+
 import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
