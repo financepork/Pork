517ea54ee135d61cccbcad3fe3dcc5675ece4b12
Bernardo Soares <bernardo.soares30@outlook.com>
2025-06-30 22:49:18 -0300

    Classes tailwind adcionadas no componente card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Card = ({ title, text, imgPath }) => {
     return (
-        <div>
-            <div>
-                <h2>{title}</h2>
-                <p>{text}</p>
+        <div className='card'>
+            <div className='flex flex-col space-y-8 items-center'>
+                <h2 className='text-[var(--color-white)] font-title text-7xl '>{title}</h2>
+                <p className='text-[var(--color-white)] font-text text-5xl'>{text}</p>
+            </div>
                 <aside>
                     <img src={imgPath} alt="" />
                 </aside>
-            </div>
         </div>
     )
 }
