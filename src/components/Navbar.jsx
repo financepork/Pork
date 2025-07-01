a107fdeaff2fbf1efb3e454d1ac4eedb088e817c
Bernardo Soares <bernardo.soares30@outlook.com>
2025-06-30 22:50:17 -0300

    Navbar estilizada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <h1>Pork</h1>
-        <ul>
-            <li>Sobre</li>
-            <li>Registrar</li>
-            <li>Login</li>
-        </ul>
+    <nav className='bg-[var(--color-green)] min-h-24 w-370 mx-10 my-8 rounded-4xl '>
+      <div className='flex justify-between text-[var(--color-white)] '>
+        <h1 className='text-5xl p-4 pl-8 ml-4 m-2 font-title'>Pork</h1>
+        <div className='m-4 text-3xl flex font-text items-center pr-12 space-x-8'>
+            <a href="$">Sobre</a>
+            <a href="$">Registrar</a>
+            <a href="$">Login</a>
+        </div>
       </div>
     </nav>
   )
