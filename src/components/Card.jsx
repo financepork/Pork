70591d1408c391c479d4c97ccb23c4bd62aa916c
Bernardo Soares <bernardo.soares30@outlook.com>
2025-07-03 14:48:58 -0300

    mudancas no card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Card = ({ title, text, imgPath }) => {
     return (
-        <div className='bg-[var(--color-green)] m-10 w-310 h-120 rounded-3xl flex justify-between p-8'>
-            <div className=''>
-                <h2 className='text-[var(--color-white)] text-7xl font-title' >{title}</h2>
-                <p className='text-[var(--color-white)] text-4xl font-text mt-12'>{text}</p>
+        <div className='bg-none m-10 w-310 h-120 rounded-3xl flex justify-between p-8 shadow-[1px_1px_25px_1px_var(--color-dark-green)]'>
+            <div>
+                <h2 className='text-[var(--color-white)] font-title leading-tight pb-3 text-7xl'>{title}</h2>
+                <p className='bg-gradient-to-b from-[var(--color-green)] to-[var(--color-light-green)] bg-clip-text text-4xl font-text mt-12 text-transparent'>{text}</p>
             </div>
-            <div className='bg-[var(--color-white)] h-100 w-550 ml-5 rounded-2xl'>
+            <div className='bg-[var(--color-green)] h-100 w-550 ml-5 rounded-2xl'>
                 <aside>
                     <img src={imgPath} alt=""/>
                 </aside>
