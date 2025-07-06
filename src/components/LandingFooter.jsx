de5306aba06726ac3d825dedeeb87abaf08bc730
Bernardo Soares <bernardo.soares30@outlook.com>
2025-07-06 12:52:38 -0300

    mudancas no landing footer

--- src/components/LandingFooter.jsx
+++ src/components/LandingFooter.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const LandingFooter = () => {
   return (
-    <div  className='bg-[var(--color-white)] min-h-100'>
-        <footer className='bg-[var(--color-white)]'>
+    <div  className='bg-[var(--color-gray)] min-h-90 border-footer'>
+        <footer >
+        <div className='flex flex-col items-center'>
+        <div className='flex justify-center items-center mt-15'>
+          <img src="./icon.png" alt="Logo Pork" className='h-25' />
+          <h1 className='text-6xl font-title mt-3 ml-1 text-[var(--color-white)]'>Pork</h1>
+        </div>
+        <hr className='bg-[var(--color-lgray)] min-w-150 h-1 mt-6' />
+        <p className='text-[var(--color-white)] mt-5 text-1xl'>© 2025 Bernardo Soares & João Vitor Chaves. Todos os direitos reservados.</p>
+        <p className='text-[var(--color-white)] mt-3'>v1.0.0</p>
+        </div>
+        
         </footer>
     </div>
   )
