c583a29f47ad3585421536838434f9ad30f53eb0
Bernardo Soares <bernardo.soares30@outlook.com>
2025-07-07 18:12:39 -0300

    NavBar e Footer mudados

--- src/components/LandingFooter.jsx
+++ src/components/LandingFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LandingFooter = () => {
   return (
-    <div  className='bg-[var(--color-gray)] min-h-90 border-footer'>
+    <div  className='bg-[var(--color-gray)] min-h-130 border-footer'>
         <footer >
         <div className='flex flex-col items-center'>
         <div className='flex justify-center items-center mt-15'>
@@ -13,7 +13,13 @@ const LandingFooter = () => {
         <p className='text-[var(--color-white)] mt-5 text-1xl'>© 2025 Bernardo Soares & João Vitor Chaves. Todos os direitos reservados.</p>
         <p className='text-[var(--color-white)] mt-3'>v1.0.0</p>
         </div>
-        
+          <div className='text-[var(--color-white)] mt-16 flex justify-center items-center'>
+            <div className='flex flex-col justify-center items-center'>
+              <h1 className='text-3xl font-title text-[var(--color-green)]'>E-mail para Contato:</h1>
+            <p className='font-text-small mt-3 text-2xl'>financepork@gmail.com</p>
+            </div>
+            
+          </div>
         </footer>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-[var(--color-gray)] min-h-23 w-auto top-0 pt-2 border-nav'>
+    <nav className='bg-[var(--color-gray)] min-h-23 w-auto top-0 pt-2 border-nav '>
       <div className='flex justify-around text-[var(--color-white)] text-1xl'>
         <div className='flex justify-center items-center'>
           <img src="./icon.png" alt="Logo Pork" className='h-18' />
           <h1 className='text-4xl font-title mt-3 ml-1'>Pork</h1>
         </div>
         <div className='flex space-x-10 items-center font-text'>
-          <a href="$" className='transform hover:scale-130 ease-in-out duration-700'>Apresentação</a>
-          <a href="$" className='transform hover:scale-130 ease-in-out duration-700'>Funcionamento</a>
-          <a href="$" className='transform hover:scale-130 ease-in-out duration-700'>Sobre Nós</a>
+          <a href="$" className='transform hover:scale-110 ease-in-out duration-700'>Apresentação</a>
+          <a href="$" className='transform hover:scale-110 ease-in-out duration-700'>Funcionamento</a>
+          <a href="$" className='transform hover:scale-110 ease-in-out duration-700'>Sobre Nós</a>
         </div>
         <div className='flex justify-center items-center  text-[var(--color-white)] font-text space-x-4'>
-          <a href="$" className='transform hover:scale-115 ease-in-out duration-400'>Login</a>
+          <a href="$" className='transform hover:scale-105 ease-in-out duration-400'>Login</a>
           <a href="$" className="border-0 text-[var(--color-black)] bg-[var(--color-white)] rounded-2xl p-3 hover:bg-[var(--color-green)] hover:text-[var(--color-white)] transition-colors duration-400 ease-in-out"><button>Registre-se</button></a>
         </div>
       </div>
