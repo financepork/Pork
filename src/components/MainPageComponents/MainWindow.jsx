99a5120e4314373d37173b487188a1dec98b8deb
chaves <chavesprogrammer@gmail.com>
2025-08-05 15:53:01 -0300

    ajuste na cor da mainWindow

--- src/components/MainPageComponents/MainWindow.jsx
+++ src/components/MainPageComponents/MainWindow.jsx
@@ -12,7 +12,7 @@ const MainWindow = ({setOpenWindow}) => {
     <main
       className=' scroll-smooth h-full w-full '>
       <div data-aos="fade-up" data-aos-delay="0" data-aos-duration="900" data-aos-easing="ease-in"
-       className='flex flex-wrap h-auto w-full bg-amber-400 rounded-t-4xl p-8 xl:p-16 gap-4 xl:gap-12  overflow-y-none justify-center'>
+       className='flex flex-wrap h-auto w-full bg-[var(--color-green)] rounded-t-4xl p-8 xl:p-16 gap-4 xl:gap-12  overflow-y-none justify-center'>
       
             <CardWindow titleText="Plano de Economia" imgSrc='planEco.png' windowOpen={'PlanejamentoEconomico'} setOpenWindow={setOpenWindow} />
       
